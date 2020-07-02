2e31e09ac8df3cafa28e0fb0f1a3be7aed4992be
vharsha50 <vharsha50@gmail.com>
2020-07-02 20:10:53 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
