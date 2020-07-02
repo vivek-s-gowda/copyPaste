d08e71cbfb9cf5287c14e1b4721a1f754ef818e4
Vivek S <vivek.s@atonarp.com>
2020-07-02 23:36:30 +0530

    added logo to asset , ui implement of contant page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RouterModule, Routes } from "@angular/router";
 const appRoutes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: "full" },
   { path: "login", component: LoginComponent },
+  { path: "content", component: ContentComponent },
 ];
 @NgModule({
   declarations: [
