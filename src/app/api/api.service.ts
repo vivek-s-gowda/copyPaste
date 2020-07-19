f7eb3b9fc68e9b92b129b478d8308961aefe3156
Vivek S <vivek.s@atonarp.com>
2020-07-19 21:56:06 +0530

    mobile compatable

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -8,7 +8,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 })
 export class ApiService {
 
-  endpoint: string = 'http://localhost:3000/api';
+  endpoint: string = 'http://copypasteserver.herokuapp.com/api';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
   errorMgmt;
   constructor(private http: HttpClient) { }
